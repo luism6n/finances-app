73e57b0f3ed4a17ba3847df3b911610c9e6c868b
Luís Möllmann <luism6n@gmail.com>
2022-06-21 03:11:35 -0300

    plot expenses, income and net

--- src/Visualization.js
+++ src/Visualization.js
@@ -16,7 +16,25 @@ export default function Visualization() {
       b: 50,
     };
 
-    const yDomain = d3.extent(transactions.map((t) => t.amount));
+    const expenses = d3.rollup(
+      transactions.filter((t) => t.amount < 0),
+      (g) => d3.sum(g, (t) => t.amount),
+      (t) => t.date.format("Y/M")
+    );
+
+    const income = d3.rollup(
+      transactions.filter((t) => t.amount >= 0),
+      (g) => d3.sum(g, (t) => t.amount),
+      (t) => t.date.format("Y/M")
+    );
+
+    const net = d3.rollup(
+      transactions,
+      (g) => d3.sum(g, (t) => t.amount),
+      (t) => t.date.format("Y/M")
+    );
+
+    const yDomain = [d3.min(expenses.values()), d3.max(income.values())];
     const yScale = d3
       .scaleLinear()
       .domain(yDomain)
@@ -28,22 +46,15 @@ export default function Visualization() {
       .attr("transform", `translate(${margin.r})`)
       .call(yAxis);
 
-    const datesExtent = d3.extent(transactions.map((t) => t.date));
-    const maxDate = datesExtent[1].clone().add(1, "month");
-    let allMonths = [datesExtent[0].clone().subtract(1, "month")];
-    let i = 0;
-    while (allMonths[i] < maxDate) {
-      allMonths.push(allMonths[0].clone().add(i, "months"));
-      i++;
-    }
-    allMonths.push(maxDate);
-
-    const xDomain = [...new Set(allMonths.map((d) => d.format("Y/M")))].sort(); // Add empty month at the end so that we have space
-    const xRange = xDomain.map(
-      (t, i) =>
-        margin.l + (i * (width - margin.r - margin.l)) / (xDomain.length - 1)
-    );
-    const xScale = d3.scaleOrdinal().domain(xDomain).range(xRange);
+    const xDomain = [
+      ...new Set(transactions.map((t) => t.date.format("Y/M"))),
+    ].sort(); // Add empty month at the end so that we have space
+    const xScale = d3
+      .scaleBand()
+      .domain(xDomain)
+      .range([margin.l, width - margin.r])
+      .paddingOuter(0.1)
+      .paddingInner(0.1);
     const xAxis = d3.axisBottom(xScale);
     d3.select("#container")
       .select("g.xAxis")
@@ -51,11 +62,41 @@ export default function Visualization() {
       .call(xAxis);
 
     const lineAtZero = d3.line()([
-      [xRange[0], yScale(0)],
-      [xRange[xRange.length - 1], yScale(0)],
+      [margin.l, yScale(0)],
+      [width - margin.r, yScale(0)],
     ]);
 
     d3.select("path.lineAtZero").attr("d", lineAtZero).style("stroke", "gray");
+
+    d3.select("g.expenses")
+      .selectAll("rect")
+      .data(expenses.entries())
+      .join("rect")
+      .attr("x", (d) => xScale(d[0]))
+      .attr("y", yScale(0))
+      .attr("height", (d) => Math.abs(yScale(0) - yScale(d[1])))
+      .attr("width", xScale.bandwidth() / 4)
+      .style("fill", "red");
+
+    d3.select("g.income")
+      .selectAll("rect")
+      .data(income.entries())
+      .join("rect")
+      .attr("x", (d) => xScale(d[0]) + xScale.bandwidth() / 4)
+      .attr("y", (d) => yScale(d[1]))
+      .attr("height", (d) => Math.abs(yScale(0) - yScale(d[1])))
+      .attr("width", xScale.bandwidth() / 4)
+      .style("fill", "green");
+
+    d3.select("g.net")
+      .selectAll("rect")
+      .data(net.entries())
+      .join("rect")
+      .attr("x", (d) => xScale(d[0]) + (2 * xScale.bandwidth()) / 4)
+      .attr("y", (d) => Math.min(yScale(0), yScale(d[1])))
+      .attr("height", (d) => Math.abs(yScale(0) - yScale(d[1])))
+      .attr("width", xScale.bandwidth() / 4)
+      .style("fill", "blue");
   }, [transactions, width, height]);
 
   return (
@@ -64,6 +105,9 @@ export default function Visualization() {
         <g className="xAxis"></g>
         <path className="lineAtZero"></path>
         <g className="yAxis"></g>
+        <g className="expenses"></g>
+        <g className="income"></g>
+        <g className="net"></g>
       </svg>
     </Stack>
   );
