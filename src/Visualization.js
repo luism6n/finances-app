e76fd13884c7b246db0a1e5337d5d60464c3dd4f
Luís Möllmann <luism6n@gmail.com>
2022-06-21 02:18:19 -0300

    add a month to each side of the xAxis

--- src/Visualization.js
+++ src/Visualization.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from "react";
+import React, { useEffect } from "react";
 import useTransactions from "./useTransactions";
 import * as d3 from "d3";
 import useSize from "./useSize";
@@ -7,7 +7,6 @@ import { Stack } from "@mui/material";
 export default function Visualization() {
   const transactions = useTransactions();
   const { ref, height, width } = useSize();
-  console.log({ height, width });
 
   useEffect(() => {
     const margin = {
@@ -17,13 +16,7 @@ export default function Visualization() {
       b: 50,
     };
 
-    const rollup = d3.rollup(
-      transactions,
-      (g) => d3.sum(g, (t) => t.amount),
-      (t) => t.date.startOf("month")
-    );
-
-    const yDomain = d3.extent(rollup.values());
+    const yDomain = d3.extent(transactions.map((t) => t.amount));
     const yScale = d3
       .scaleLinear()
       .domain(yDomain)
@@ -35,23 +28,41 @@ export default function Visualization() {
       .attr("transform", `translate(${margin.r})`)
       .call(yAxis);
 
-    const xDomain = d3.extent(rollup.keys());
-    const xScale = d3
-      .scaleTime()
-      .domain(xDomain)
-      .range([margin.l, width - margin.r])
-      .nice();
+    const datesExtent = d3.extent(transactions.map((t) => t.date));
+    const maxDate = datesExtent[1].clone().add(1, "month");
+    let allMonths = [datesExtent[0].clone().subtract(1, "month")];
+    let i = 0;
+    while (allMonths[i] < maxDate) {
+      allMonths.push(allMonths[0].clone().add(i, "months"));
+      i++;
+    }
+    allMonths.push(maxDate);
+
+    const xDomain = [...new Set(allMonths.map((d) => d.format("Y/M")))].sort(); // Add empty month at the end so that we have space
+    const xRange = xDomain.map(
+      (t, i) =>
+        margin.l + (i * (width - margin.r - margin.l)) / (xDomain.length - 1)
+    );
+    const xScale = d3.scaleOrdinal().domain(xDomain).range(xRange);
     const xAxis = d3.axisBottom(xScale);
     d3.select("#container")
       .select("g.xAxis")
       .attr("transform", `translate(0, ${height - margin.b})`)
       .call(xAxis);
+
+    const lineAtZero = d3.line()([
+      [xRange[0], yScale(0)],
+      [xRange[xRange.length - 1], yScale(0)],
+    ]);
+
+    d3.select("path.lineAtZero").attr("d", lineAtZero).style("stroke", "gray");
   }, [transactions, width, height]);
 
   return (
     <Stack sx={{ flex: 1, height: "100%" }} ref={ref}>
       <svg height="100%" width="100%" id="container">
         <g className="xAxis"></g>
+        <path className="lineAtZero"></path>
         <g className="yAxis"></g>
       </svg>
     </Stack>
